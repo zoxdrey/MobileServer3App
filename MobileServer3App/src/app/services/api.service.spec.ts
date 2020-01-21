9e7737cfad788194045a7456424eafb09cdc4c01
Вячеслав Петров <zoxypva@gmail.com>
2020-01-21 23:21:27 +0300

    add auth interceptor

--- MobileServer3App/src/app/services/api.service.spec.ts
+++ MobileServer3App/src/app/services/api.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HttpService } from './http.service';
+import { ApiService } from './api.service';
 
-describe('HttpService', () => {
+describe('ApiService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: HttpService = TestBed.get(HttpService);
+    const service: ApiService = TestBed.get(ApiService);
     expect(service).toBeTruthy();
   });
 });
